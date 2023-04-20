931263536297bc0fc4bc54e770b6a5bc96913387
Moe Zaky <devmuzaky@gmail.com>
2023-04-20 21:47:04 +0200

    isolating login form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import {AuthModalComponent} from './auth-modal/auth-modal.component';
 import {SharedModule} from "../shared/shared.module";
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AuthModalComponent
-  ],
-  exports: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    AuthModalComponent
   ]
 })
 export class UserModule {
