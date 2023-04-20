156da317bf7daa97e466a473d2aa04146a2bad70
Moe Zaky <devmuzaky@gmail.com>
2023-04-20 02:38:38 +0200

    creating Auth component

---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.scss']
+})
+export class AuthModalComponent {
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,8 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthModalComponent
+  ],
+  exports: [
+    AuthModalComponent
+  ],
   imports: [
     CommonModule
   ]
