f0185a93ec2bd0d5393521f4eceabced5461181f
Moe Zaky <devmuzaky@gmail.com>
2023-04-20 02:03:19 +0200

    installing tailwind and configure it

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // require('postcss-import'),
+    // require('tailwindcss'),
+    // require('autoprefixer'),
+  ],
+}
+
