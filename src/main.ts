f72bdef76769b0382fba77cba87505e8e4d366af
moe_zaky <devmuzaky@gmail.com>
2023-04-26 16:03:44 +0200

    making delaying until the app retrieve the token from Firebase

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from "@angular/core";
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import firebase from "firebase/compat/app";
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
+import {environment} from "./environments/environment";
+import 'firebase/compat/auth'
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+firebase.initializeApp(environment.firebase)
+
+
+let appInitialized = false;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!appInitialized) {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+    console.log('App initialized!')
+
+  }
+  appInitialized = true;
+})
