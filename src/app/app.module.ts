d049c42e9f5cc16b361e49188c83dcecd1b307e8
moe_zaky <devmuzaky@gmail.com>
2023-04-26 09:48:22 +0200

    installing firebase into the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {BrowserModule} from '@angular/platform-browser';
+import {environment} from "../environments/environment";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {UserModule} from "./user/user.module";
-import { NavComponent } from './nav/nav.component';
+import {NavComponent} from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +18,12 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule
+    UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCBqyEX7euM3tDZEaRQ9wdgAVJnYUS-FPU",
+    authDomain: "clips-bb4a9.firebaseapp.com",
+    projectId: "clips-bb4a9",
+    storageBucket: "clips-bb4a9.appspot.com",
+    messagingSenderId: "410413021675",
+    appId: "1:410413021675:web:5cb345f14d1a5d79fa41ae",
+    measurementId: "G-HEXNXK7H7B"
+  }
+};
