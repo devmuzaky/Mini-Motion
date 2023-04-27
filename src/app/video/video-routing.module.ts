722e08cbfe7416c888962b15bd96d88b4b59f80a
moe_zaky <devmuzaky@gmail.com>
2023-04-27 12:38:12 +0200

    finishing upload/feature

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ManageComponent} from "./manage/manage.component";
+import {UploadComponent} from "./upload/upload.component";
 
 const routes: Routes = [
   {
-    path:'manage',
+    path: 'manage',
     component: ManageComponent,
+    data: {
+      authOnly: true
+    }
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
     data: {
       authOnly: true
     }
@@ -16,4 +24,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class VideoRoutingModule { }
+export class VideoRoutingModule {
+}
