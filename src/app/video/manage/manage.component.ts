e17c45e3f3d0d98043b7d5885e24fafa2994cd87
moe_zaky <devmuzaky@gmail.com>
2023-04-27 14:08:28 +0200

    setting query params for sorting data

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-manage',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class ManageComponent {
 
+  videoOrder = '1';
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe((params: Params) => {
+      // this.videoOrder = params['sort'] === '2' ? '1' : '0';
+      this.videoOrder =  params['sort'] === '2' ? params['sort'] : '1';
+    })
+  }
+
+  sortVideos($event: Event) {
+    $event.preventDefault();
+    const {value} = $event.target as HTMLSelectElement;
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value
+      }
+    })
+
+
+  }
 }
