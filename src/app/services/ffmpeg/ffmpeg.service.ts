ef64a7177fea59dc1950cce6d1fdc3c7a8cec2aa
moe_zaky <devmuzaky@gmail.com>
2023-05-03 16:46:15 +0300

    creating screenshots url

--- src/app/services/ffmpeg/ffmpeg.service.ts
+++ src/app/services/ffmpeg/ffmpeg.service.ts
@@ -49,6 +49,23 @@ export class FfmpegService {
       ...commands
     )
 
+    let screenshots: string[] = []
+
+    seconds.forEach(second => {
+      const screenshotFile = this.ffmpeg.FS('readFile', `output_0${second}.png`)
+      const screenshotBlob = new Blob(
+        [screenshotFile.buffer],
+        {
+          type: 'image/png'
+        }
+      )
+      const screenshotUrl = URL.createObjectURL(screenshotBlob)
+
+      screenshots.push(screenshotUrl)
+    })
+
+    return screenshots;
+
   }
 }
 
