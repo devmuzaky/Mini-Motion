1090983740b67c13132cf453e59c8a498f6a5c55
moe_zaky <devmuzaky@gmail.com>
2023-05-03 16:38:49 +0300

    Generating MULTIPLE screenshots

--- src/app/services/ffmpeg/ffmpeg.service.ts
+++ src/app/services/ffmpeg/ffmpeg.service.ts
@@ -26,30 +26,27 @@ export class FfmpegService {
 
   async getScreenShots(file: File) {
     if (!this.isReady) {
-      await this.init(); // if ffmpeg is not ready, wait for it to be ready before we use it (this is an async function so we need to await it)
+      await this.init();
     }
 
-    const data = await fetchFile(file); // create a file-like object from the file that we can use with ffmpeg
-    // FS is a virtual file system that ffmpeg uses, we need to write the file to the virtual file system before we can use it with ffmpeg
+    const data = await fetchFile(file);
     this.ffmpeg.FS('writeFile', file.name, data);
 
-    /*
-    *  ffmpeg command to get screenshots from a video
-    * -i tell FFmpeg grab a specific file from file system
-    *
-    * */
+    const seconds = [1, 2, 3]
+    const commands: string [] = []
+
+    seconds.forEach(second => {
+      commands.push(
+        '-ss', `00:00:0${second}`,
+        '-frames:v', '1',
+        '-filter:v', 'scale=510:-1',
+        `output_0${second}.png`
+      )
+    })
 
     await this.ffmpeg.run(
-      // Input
-      '-i', file.name, // must correspond to the file name we used when we wrote the file to the virtual file system
-      // Output options
-      '-ss', '00:00:01', // start time
-      '-frames:v', '1', // number of frames to output (1 in this case) (v stands for video) (this is optional, if you don't specify this, ffmpeg will output 25 frames by default)
-      '-filter:v', 'scale=510:-1', // scale the image to 320px wide (the height will be calculated automatically to preserve the aspect ratio)
-      // Output
-      'output_01.png' // output file name (this will be written to the virtual file system)
-
-
+      '-i', file.name,
+      ...commands
     )
 
   }
